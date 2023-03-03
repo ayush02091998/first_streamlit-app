b88a0643423a4eb6577e39d79beb496516978760
Ayush pattiwar <69310058+ayush02091998@users.noreply.github.com>
2023-03-03 15:28:22 +0530

    Update streamlit_app.py
    Added new Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard Bolied Free-Range Egg')
 streamlit.text('🥑🍞Avacado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
