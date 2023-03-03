4ff2cc0b3c9ba8b2d5998a9d5cd21d3786fa6178
Ayush pattiwar <69310058+ayush02091998@users.noreply.github.com>
2023-03-03 15:27:24 +0530

    Update streamlit_app.py
    Added Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ streamlit.title('My Parent New Healthy Dinner')
 
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard Bolied Free-Range Egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard Bolied Free-Range Egg')
+streamlit.text('🥑🍞Avacado Toast')
+
