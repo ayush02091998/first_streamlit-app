302db6d76d2841f2bb81c4035a57587d4f215282
Ayush pattiwar <69310058+ayush02091998@users.noreply.github.com>
2023-03-03 15:24:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parent New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-stremlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard Bolied Free-Range Egg')
